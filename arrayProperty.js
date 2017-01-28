5948b1c6e2c501e3cb1b0479d4ea07d2260fa685
dmayes403 <davidrmayes@gmail.com>
2017-01-28 12:06:27 -0700

    finished constructor conductor

--- arrayProperty.js
+++ arrayProperty.js
@@ -1,5 +1,24 @@
 //Just like you can add methods to your own constructor, you can also add methods and properties to built in classes in JavaScript like Arrays and Objects.
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
-  
+
   //code here
+
+// String.prototype.reverse = function(){
+//   console.log('test');
+//   stringArr = [];
+//   string = this.split('');
+//   for(var i = 0;i<string.length;i++){
+//     stringArr.unshift(string[i]);
+//   }
+//   stringArr = stringArr.join('');
+//   return stringArr;
+// }
+//
+//
+// return this.split('').reverse().join('');
+
+
+String.prototype.reverse = function(){
+  return this.split('').reverse().join('');
+}
--- instanceArray.js
+++ instanceArray.js
@@ -1,5 +1,5 @@
 /*
-  Your 3 Users will be the following. 
+  Your 3 Users will be the following.
     0) Tyler, tylermcginnis33@gmail.com, 'iLoveJS'
     1) Cahlan, cahlan@devmounta.in, 'iLoveHashtags'
     2) Lenny, lenny@theLenster.com, 'iLoveLentilSoup'
@@ -14,28 +14,38 @@ var User = function(name, email, pw){
 //Create an Array called 'users' that will store all our instances of User.
 
   //code here
-
+var users = [];
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
   //code here
 
-console.log('Tyler\'s information is ');
+var Tyler = new User('Tyler', 'tylermcginnis33@gmail.com','iLoveJS');
+var Cahlan = new User('Cahlan', 'cahlan@devmounta.in','iLoveHashtags');
+var Lenny = new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup');
+
+users.push(Tyler, Cahlan, Lenny);
+
+console.log('Tyler\'s information is ' + users[0].name + ', ' + users[0].email + ', ' + users[0].pw);
 //Console.log all of Tylers information
 
   //code here
 
-console.log('Lenny\'s information is ');
+console.log('Lenny\'s information is ' + users[1].name + ', ' + users[1].email + ', ' + users[1].pw);
 //Now console.log all of Lennys information
 
   //code here
 
-
+var Dave = new User('Dave', ' DaveMayes@gmail.com', 'iLoveToCode');
+users.push(Dave);
 //Now create another instance of User using your own information and then add that to your users array.
 
   //code here
 
 console.log('All my users names are ');
-//Now loop through your users Array and console.log every users name. 
+//Now loop through your users Array and console.log every users name.
 
   //code here
+for(var i = 0;i< users.length; i++){
+  console.log(users[i]);
+}
